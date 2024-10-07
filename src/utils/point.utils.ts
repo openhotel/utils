15e6e0c5f5b3cca9ac5b0c92c21c02e8b48e1cc1
pagoru <pagoru@gmail.com>
2024-10-07 22:35:05 +0200

    more

--- src/utils/point.utils.ts
+++ src/utils/point.utils.ts
@@ -4,7 +4,7 @@ export const isPoint3dEqual = (
   pointA: Point3d,
   pointB: Point3d,
   ignoreY: boolean = true,
-) =>
+): boolean =>
   pointA?.x === pointB?.x &&
   (pointA?.y === pointB?.y || ignoreY) &&
   pointA?.z === pointB?.z;
