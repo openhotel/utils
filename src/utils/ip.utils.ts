0babe451800bc7e9ea616e3561f9a213cce2053e
Pablo González Rubio <pagoru@gmail.com>
2024-10-09 11:18:07 +0200

    feat: added compare ips func - fix #1 (#2)

--- src/utils/ip.utils.ts
+++ src/utils/ip.utils.ts
@@ -8,3 +8,11 @@ export const getIpFromUrl = async (url: string): Promise<string> => {
   const { hostname } = getURL(url);
   return (await Deno.resolveDns(hostname, "A"))[0];
 };
+
+export const compareIps = (ip: string, remoteIp: string): boolean =>
+  //check if is local network
+  remoteIp.startsWith("192.168.") ||
+  //check if is local computer
+  remoteIp.startsWith("172.") ||
+  //check if is equal
+  ip === remoteIp;
