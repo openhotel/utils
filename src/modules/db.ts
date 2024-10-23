fd97b17985e3e115f5725aa5113d4376f4ac036f
pagoru <pagoru@gmail.com>
2024-10-23 15:29:25 +0200

    feat: added migrations - #5

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -3,16 +3,84 @@ import type {
   DbKey,
   DbListOptions,
   DbListSelector,
+  DbMigrationsMutable,
   DbMutable,
   DbProps,
+  Migration,
   ResultItem,
 } from "../types/db.types.ts";
 
-export const getDb = ({ pathname }: DbProps): DbMutable => {
+export const getDb = (
+  { pathname }: DbProps = { pathname: "./database" },
+): DbMutable => {
   let db: Deno.Kv;
 
+  const $migrations: DbMigrationsMutable = ((): DbMigrationsMutable => {
+    let migrationList: Migration[] = [];
+
+    const load = async (migrations: Migration[]) => {
+      console.log("> Loading migrations...");
+
+      const migrationsIdList = migrations.map(({ id }) => id);
+
+      const currentList: string[] = [];
+      const repeatedList: string[] = [];
+      for (const migrationId of migrationsIdList)
+        currentList.includes(migrationId)
+          ? repeatedList.push(migrationId)
+          : currentList.push(migrationId);
+
+      if (repeatedList.length)
+        throw `Migrations need to have unique ids || Repeated ids: ${repeatedList.join(",")}`;
+
+      migrationList = migrations;
+
+      let { list, index } = ((await get(["__migrations"])) ?? {
+        list: [],
+        index: -1,
+      }) as { list: string[]; index: number };
+
+      let migrationError;
+      for (const migration of migrationList) {
+        if (list.includes(migration.id)) {
+          console.log(`- Migration ${migration.id} was already applied!`);
+          continue;
+        }
+        try {
+          await migration.up(dbMutables);
+          list.push(migration.id);
+          index++;
+          console.log(`> Migration ${migration.id} applied (${index})!`);
+        } catch (e) {
+          migrationError = [`> Migration ${migration.id} failed!`, e];
+          break;
+        }
+      }
+
+      await set(["__migrations"], { list, index });
+
+      if (migrationError) throw `${migrationError[0]} | ${migrationError[1]}`;
+
+      console.log("> Migrations done!");
+    };
+
+    const up = async (count = 1) => {
+      //TODO
+    };
+
+    const down = async (count = 1) => {
+      //TODO
+    };
+
+    return {
+      load,
+      up,
+      down,
+    };
+  })();
+
   const load = async () => {
-    db = await Deno.openKv(pathname ?? "./database");
+    db = await Deno.openKv(pathname);
   };
 
   const $checkDbNull = () => {
@@ -76,7 +144,7 @@ export const getDb = ({ pathname }: DbProps): DbMutable => {
     db = null;
   };
 
-  return {
+  const dbMutables = {
     get,
     set,
     list,
@@ -85,5 +153,8 @@ export const getDb = ({ pathname }: DbProps): DbMutable => {
 
     load,
     close,
+
+    migrations: $migrations,
   };
+  return dbMutables;
 };
