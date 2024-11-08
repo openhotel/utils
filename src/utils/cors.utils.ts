8d7fb6d5d0077d7119ef6f66135d0d6a3134c757
Pablo González Rubio <pagoru@gmail.com>
2024-11-08 08:36:16 +0100

    fix: added unit tests core - fix #8 (#10)
    * fix: added unit tests core - fix #8
    
    * fix: added unit tests core - fix #8
    
    * fix: added unit tests core - fix #8
    
    * fix: added unit tests core - fix #8

--- src/utils/cors.utils.ts
+++ src/utils/cors.utils.ts
@@ -1,19 +1,11 @@
-const HEADERS = [
-  ["Access-Control-Allow-Origin", "*"],
-  ["Access-Control-Allow-Methods", "GET, POST, PUT", "DELETE"],
-  [
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  ],
-  ["Access-Control-Allow-Credentials", "true"],
-];
+import { CORS_HEADERS } from "../consts/cors.consts.ts";
 
 export const getCORSHeaders = (): Headers => {
   const headers = new Headers();
-  for (const [key, value] of HEADERS) headers.append(key, value);
+  for (const [key, value] of CORS_HEADERS) headers.append(key, value);
   return headers;
 };
 1;
 export const appendCORSHeaders = (headers: Headers): void => {
-  for (const [key, value] of HEADERS) headers.append(key, value);
+  for (const [key, value] of CORS_HEADERS) headers.append(key, value);
 };
