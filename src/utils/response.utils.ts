3d10b029d382437229b84130d09122d7a467aea6
José Alvarado <aaje.dev@gmail.com>
2024-12-05 09:59:48 -0600

    feat: Add HTTP response utility methods for standard status codes (#30)
    * http utility methods
    
    * Update response.utils.ts
    
    * prettier
    
    * prettier
    
    ---------
    
    Co-authored-by: Pablo González Rubio <pagoru@gmail.com>

---
+++ src/utils/response.utils.ts
@@ -0,0 +1,20 @@
+import { HttpStatusCode } from "../enums/http-status-code.enums.ts";
+
+/**
+ * Sends a JSON response with a specified HTTP status and additional data.
+ *
+ * @param {HTTPStatusCode} status - The HTTP status code to be returned in the response.
+ * @param {any} data - The additional data to include in the response body. This data is spread into the response object.
+ *
+ * @returns {Response} A JSON response containing the `statusCode`, a easy readable `status` message,
+ * and any additional data provided.
+ *
+ * @example
+ * Example usage
+ * const response = getResponse(200, { message: "Request successful" });
+ * Returns a JSON response with { "status": 200, "data": { "message": "Request successful" } }
+ */
+export const getResponse = <Data>(
+  status: HttpStatusCode,
+  data?: Data,
+): Response => Response.json({ status, ...(data || {}) }, { status });
