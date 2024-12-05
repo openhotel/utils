1fb1e1d030356660915f65b08595db2c0ff35f94
pagoru <pagoru@gmail.com>
2024-12-05 16:51:28 +0100

    prettier

--- src/enums/main.ts
+++ src/enums/main.ts
@@ -1,5 +1,5 @@
 export * from "./direction.enums.ts";
-export * from "./http-status-code.enums.ts"
-export * from "./http-status-message.enums.ts"
+export * from "./http-status-code.enums.ts";
+export * from "./http-status-message.enums.ts";
 export * from "./os.enum.ts";
 export * from "./request.enum.ts";
