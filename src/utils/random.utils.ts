161b6bad2ea47fddccdae8b7692e36ca415eed66
whiskoder <whiskoder@gmail.com>
2024-12-09 04:10:44 -0600

    generate a secure random string

--- src/utils/random.utils.ts
+++ src/utils/random.utils.ts
@@ -6,11 +6,11 @@ export const getRandomString = (length: number): string => {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
+  const randomValues = new Uint32Array(length);
+  crypto.getRandomValues(randomValues);
+  randomValues.forEach(
+    (value) => (result += characters.charAt(value % charactersLength)),
+  );
   return result;
 };
 
