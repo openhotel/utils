dd2c7b05019343d6010cfc03626a81ee1e9fa7f1
Pablo González Rubio <pagoru@gmail.com>
2024-12-13 23:39:42 +0100

    feat: add patch as cors headers - fix #49 (#50)

--- src/consts/cors.consts.ts
+++ src/consts/cors.consts.ts
@@ -1,6 +1,6 @@
 export const CORS_HEADERS = [
   ["Access-Control-Allow-Origin", "*"],
-  ["Access-Control-Allow-Methods", "GET, POST, PUT", "DELETE"],
+  ["Access-Control-Allow-Methods", "GET, POST, PUT", "PATCH", "DELETE"],
   [
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept",
