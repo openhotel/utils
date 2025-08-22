307318c0df77220009ca1fafcd5e21b3652e345f
Pablo González Rubio <pagoru@gmail.com>
2025-08-22 15:38:52 +0200

    fix: remove get local path from import - fix #109 (#110)

--- src/utils/path.utils.ts
+++ src/utils/path.utils.ts
@@ -3,14 +3,5 @@ import * as path from "@std/path";
 export const getExecPath = (): string => Deno.execPath();
 export const getPath = (): string => path.dirname(getExecPath());
 
-const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
-
-export const getLocalPath = (filePath: string): string => {
-  if (filePath.startsWith("/") || /^[A-Za-z]:\\/.test(filePath)) {
-    return filePath; // already absolute
-  }
-  return path.join(__dirname, filePath);
-};
-
 export const getModulePath = (filePath: string): string =>
   path.join(path.dirname(path.fromFileUrl(Deno.mainModule)), filePath);
