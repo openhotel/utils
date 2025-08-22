970d2f64f8020b3afb1fe2ecd0c1b51c6b4c44c4
Pablo González Rubio <pagoru@gmail.com>
2025-08-22 18:25:40 +0200

    fix: updater + db have incorrect module paths - fix #113 (#115)

--- src/utils/path.utils.ts
+++ src/utils/path.utils.ts
@@ -3,5 +3,5 @@ import * as path from "@std/path";
 export const getExecPath = (): string => Deno.execPath();
 export const getPath = (): string => path.dirname(getExecPath());
 
-export const getModulePath = (filePath: string): string =>
+export const getModulePath = (filePath: string = ""): string =>
   path.join(path.dirname(path.fromFileUrl(Deno.mainModule)), filePath);
