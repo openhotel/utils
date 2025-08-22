419033829ad82bd0d77fcebe04b58f4e9723aa6f
Pablo González Rubio <pagoru@gmail.com>
2025-08-22 15:27:05 +0200

    fix: config path + yaml write/read path relative to module execution - fix #107 (#108)
    * fix: config path + yaml write/read path relative to module execution - fix #107
    
    * fix: config path + yaml write/read path relative to module execution - fix #107

--- src/modules/yaml.ts
+++ src/modules/yaml.ts
@@ -1,5 +1,6 @@
 import { parse, stringify } from "@std/yaml";
 import { createDirectoryIfNotExists } from "../utils/directory.utils.ts";
+import { getModulePath } from "../utils/path.utils.ts";
 
 export type ReadProps = {
   decode?: boolean;
@@ -16,7 +17,7 @@ export const readYaml = async <T extends unknown>(
   filePath: string,
   { decode = false }: ReadProps = { decode: false },
 ): Promise<T | unknown> => {
-  let content = await Deno.readTextFile(filePath);
+  let content = await Deno.readTextFile(getModulePath(filePath));
   if (decode)
     content = decoder.decode(new Uint8Array(content.split(",").map(Number)));
   return parse(content);
@@ -29,11 +30,12 @@ export const writeYaml = async <T extends any>(
     encode: false,
   },
 ): Promise<void> => {
-  await createDirectoryIfNotExists(filePath);
+  await createDirectoryIfNotExists(getModulePath(filePath));
 
   let content = stringify(data);
   //@ts-ignore
   if (encode) content = encoder.encode(content);
 
-  await Deno.writeTextFile(filePath, content);
+  console.log(getModulePath(filePath), content);
+  await Deno.writeTextFile(getModulePath(filePath), content);
 };
