540eda05a50ca4f92c22876da1ec83920616d38b
pagoru <pagoru@gmail.com>
2026-02-02 11:55:01 +0100

    feat: add copy directoryu utility - fix #120

--- src/utils/path.utils.ts
+++ src/utils/path.utils.ts
@@ -17,7 +17,7 @@ export const getModulePath = (filePath: string = ""): string => {
   return path.isAbsolute(filePath) ? filePath : path.join(root, filePath);
 };
 
-export const copyDirectory = async (srcPath: string, destPath: string)=> {
+export const copyDirectory = async (srcPath: string, destPath: string) => {
   await Deno.mkdir(destPath, { recursive: true });
 
   for await (const entry of Deno.readDir(srcPath)) {
@@ -30,4 +30,4 @@ export const copyDirectory = async (srcPath: string, destPath: string)=> {
       await Deno.copyFile($srcPath, $destPath);
     }
   }
-}
+};
